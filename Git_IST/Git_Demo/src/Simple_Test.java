fbc2d0280337ff6b7ff32cebe1a6a1fbb43d76ba
Akshay <akshayharugop@gmail.com>
2022-10-16 15:41:17 +0530

    IST person worked and Commited the file

--- Git_IST/Git_Demo/src/Simple_Test.java
+++ Git_IST/Git_Demo/src/Simple_Test.java
@@ -4,5 +4,8 @@ public class Simple_Test {
 	public static void main(String[] args) {
 		//IST Zone
 		System.out.println("Hello to the Git World!!!");
+		System.out.println("IST:1");
+		System.out.println("IST:2");
+		System.out.println("IST:3");
 	}
 }
